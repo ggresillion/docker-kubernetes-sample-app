65ca7ec2e5f85f75acedc31abb1fd313204b43ad
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2023-06-14 11:41:45 +0200

    updated deployment

--- backend/main.go
+++ backend/main.go
@@ -44,7 +44,7 @@ func main() {
 
 	http.HandleFunc("/", getRoot)
 	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,10 +2,6 @@
     async function fetchMessage(): Promise<string> {
         const res = await fetch("/api");
         const json: { message: string } = await res.json();
-        console.log(json)
-        console.log(typeof json)
-        console.log(json.message)
-        console.log(typeof json.message)
         return json.message;
     }
 </script>
